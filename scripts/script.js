f911884f74f36df2bc7405623dff6af71ae9db10
EricNwt <skilobaloba@gmail.com>
2025-01-07 12:03:18 -0300

    Enviando site para o github

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+function message(){
+    swal({
+        title: "Arena", 
+        text: "Nossa arena possui uma área de 525m² entre obstáculos e mata, por ser um CQB (Close Quarters Battle) nossos jogos serão de no mínimo 2 contra 2 e no máximo 4 contra 4 serão partidas Only pistol, traduzindo “Só Pistolas”, trazendo assim muito mais adrenalina por ser um jogo com muita rapidez, agilidade e um pouquinho de dor ao ser alvejado rsrsrsrs. Teremos 3 modos de jogos disponíveis o Mata-Mata, Capture a bandeira e o Bombsite!"
+    });
+}
+function message2(){
+    swal({
+        title: "O que é airsoft?",
+        text: "O airsoft é um esporte de simulação militar que envolve o uso de armas de airsoft, que são réplicas de armas de fogo reais, mas disparam projéteis não letais, geralmente esferas de plástico com 6 ou 8 mm de diâmetro. \n\n É um jogo cujo pilar principal é a HONRA dos jogadores. Assim, quando um jogador for atingido, imediatamente ele acusa o acerto e sai do jogo. A honestidade dos jogadores é então, fundamental para o desenrolar do esporte e sua popularização pelo mundo.",
+    });
+}
